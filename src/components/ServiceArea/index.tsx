cfe3ebceb439ec2a27f3f871ec28cbec1d174e65
Kmzf777 <henriqueferreira.fox@gmail.com>
2023-03-15 17:55:08 -0300

    criar componente da ServiceBox

--- src/components/ServiceArea/index.tsx
+++ src/components/ServiceArea/index.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import "./ServiceArea.css";
 import "./ServiceArea-Mobile.css";
 
-import Grafico from "../../assets/grafico.png";
 import GestaoRedesSociais from "../../assets/gestao_redes_sociais.png";
 import Marketing from "../../assets/marketing.png";
 import DesenvolvimentoWeb from "../../assets/desenvolvimento-web.png";
 import DesignerECriacao from "../../assets/designer_e_criacao.png";
 
 import video from "../../assets/Inbound.mp4";
+import ServiceBox from "../ServiceBox";
 
 const ServiceArea = () => {
   return (
@@ -19,60 +19,46 @@ const ServiceArea = () => {
         </h2>
       </div>
       <main className="service-division">
-        <div className="service-box">
-          <h2>Gestão de Redes Sociais</h2>
-          <div className="service-img">
-            <img src={GestaoRedesSociais} alt="Gráfico Redes Sociais" />
-          </div>
-
-          <h3>ANÁLISE COMPLETA, BENCHMARKETING E PLANO ESTRUTURAL</h3>
-        </div>
-        <div className="service-box">
-          <h2>Marketing Estrategico e SEO</h2>
-          <div className="service-img">
-            <img src={Marketing} alt="Gráfico Marketing" />
-          </div>
-          <h3>ESTRATEGIA FOCADA EM MARKETING E SEO</h3>
-        </div>
-        <div className="service-box">
-          <h2>Desenvolvimento Web</h2>
-          <div className="service-img">
-            <img src={DesenvolvimentoWeb} alt="Desenvolvimento Web" />
-          </div>
-          <h3>DESENVOLVIMENTO DE LANDING PAGES E-COMMERCE E OUTROS</h3>z
-        </div>
-        <div className="service-box">
-          <h2>Designer e Criação Publicitária</h2>
-          <div className="service-img">
-            <img src={DesignerECriacao} alt="PC Designer" />
-          </div>
-          <h3>CRIAÇÃO DE IDENTIDADES VISUAIS E SUAS DEMANDAS</h3>
-        </div>
+        <ServiceBox
+          titulo="Gestão de Redes Sociais"
+          thumbnail={GestaoRedesSociais}
+          desc="ANÁLISE COMPLETA, BENCHMARKETING E PLANO ESTRUTURAL"
+        />
+        <ServiceBox
+          titulo="Marketing Estrategico e SEO"
+          thumbnail={Marketing}
+          desc="ESTRATEGIA FOCADA EM MARKETING E SEO"
+        />
+        <ServiceBox
+          titulo="Desenvolvimento Web"
+          thumbnail={DesenvolvimentoWeb}
+          desc="DESENVOLVIMENTO DE LANDING PAGES E-COMMERCE E OUTROS"
+        />
+        <ServiceBox
+          titulo="Designer e Criação Publicitária"
+          thumbnail={DesignerECriacao}
+          desc="CRIAÇÃO DE IDENTIDADES VISUAIS E SUAS DEMANDAS"
+        />
       </main>
 
       <main className="gestao-area">
         <main className="gestao-text">
-          <h2><span> AUMENTE SUA </span> 
-          <div className="eficiencia">
-          <h2>EFICIÊNCIA</h2>
-          <h2>EFICIÊNCIA</h2>
-          <h2>EFICIÊNCIA</h2>
-          <h2>EFICIÊNCIA</h2>
-          <h2>EFICIÊNCIA</h2>
-
-
-
-
-          </div> <span>OPERACIONAL</span></h2>
+          <h2>
+            <span> AUMENTE SUA </span>
+            <div className="eficiencia">
+              <h2>EFICIÊNCIA</h2>
+              <h2>EFICIÊNCIA</h2>
+              <h2>EFICIÊNCIA</h2>
+              <h2>EFICIÊNCIA</h2>
+              <h2>EFICIÊNCIA</h2>
+            </div>{" "}
+            <span>OPERACIONAL</span>
+          </h2>
         </main>
         <main className="gestao-deco">
-          <div className="first-box">
-          </div>
+          <div className="first-box"></div>
           <div className="last-box"></div>
-          <div className="box-img">
-            
-          </div>
-          
+          <div className="box-img"></div>
         </main>
       </main>
 
@@ -81,13 +67,23 @@ const ServiceArea = () => {
           <h2>Bla Bla Bla</h2>
           <h3>bla bla bla</h3>
           <img src={DesignerECriacao} alt="" />
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab molestias dignissimos repellendus laboriosam distinctio unde eum nisi magnam hic tempora. Dolorem obcaecati minus totam vero provident fugiat, dolorum dicta quod.</p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab
+            molestias dignissimos repellendus laboriosam distinctio unde eum
+            nisi magnam hic tempora. Dolorem obcaecati minus totam vero
+            provident fugiat, dolorum dicta quod.
+          </p>
         </div>
         <div className="info-box">
           <h2>Bla Bla Bla</h2>
           <h3>bla bla bla</h3>
           <img src={DesignerECriacao} alt="" />
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab molestias dignissimos repellendus laboriosam distinctio unde eum nisi magnam hic tempora. Dolorem obcaecati minus totam vero provident fugiat, dolorum dicta quod.</p>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab
+            molestias dignissimos repellendus laboriosam distinctio unde eum
+            nisi magnam hic tempora. Dolorem obcaecati minus totam vero
+            provident fugiat, dolorum dicta quod.
+          </p>
         </div>
       </main>
 
---
+++ src/components/ServiceBox/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./ServiceBox.css";
+
+interface IProps {
+    titulo: string;
+    thumbnail: string;
+    desc: string;
+}
+
+const ServiceBox: React.FC<IProps> = ({titulo, thumbnail, desc}) => {
+  return (
+    <div className="service-box">
+    <h2>{titulo}</h2>
+    <div className="service-img">
+      <img src={thumbnail} alt={titulo} />
+    </div>
+
+    <h3>{desc}</h3>
+  </div>
+  )
+}
+
+export default ServiceBox
